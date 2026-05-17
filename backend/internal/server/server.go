dbdc1168c86731e165829834ceb045a91c027111
Fearcon <kevinsinn01@gmail.com>
2026-05-17 16:28:26 +0200

    feat: server adjusted

--- backend/internal/server/server.go
+++ backend/internal/server/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"strconv"
@@ -10,30 +11,56 @@ import (
 	_ "github.com/joho/godotenv/autoload"
 
 	"backend/internal/database"
+	"backend/internal/handler"
+	"backend/internal/repository"
+	"backend/internal/service"
+	"backend/migrations"
 )
 
 type Server struct {
-	port int
-
-	db database.Service
+	port        int
+	db          database.Service
+	jwtSecret   string
+	authHandler *handler.AuthHandler
+	userHandler *handler.UserHandler
+	gameHandler *handler.GameHandler
 }
 
 func NewServer() *http.Server {
 	port, _ := strconv.Atoi(os.Getenv("PORT"))
-	NewServer := &Server{
-		port: port,
 
-		db: database.New(),
+	jwtSecret := os.Getenv("JWT_SECRET")
+	if jwtSecret == "" {
+		log.Fatal("JWT_SECRET environment variable is not set")
 	}
 
-	// Declare Server config
-	server := &http.Server{
-		Addr:         fmt.Sprintf(":%d", NewServer.port),
-		Handler:      NewServer.RegisterRoutes(),
+	dbService := database.New()
+
+	if err := database.RunMigrations(dbService.DB(), migrations.FS); err != nil {
+		log.Fatalf("database migration failed: %v", err)
+	}
+
+	userRepo := repository.NewUserRepository(dbService.DB())
+	gameRepo := repository.NewGameRepository(dbService.DB())
+
+	authSvc := service.NewAuthService(userRepo, jwtSecret)
+	userSvc := service.NewUserService(userRepo)
+	gameSvc := service.NewGameService(gameRepo)
+
+	srv := &Server{
+		port:        port,
+		db:          dbService,
+		jwtSecret:   jwtSecret,
+		authHandler: handler.NewAuthHandler(authSvc),
+		userHandler: handler.NewUserHandler(userSvc),
+		gameHandler: handler.NewGameHandler(gameSvc),
+	}
+
+	return &http.Server{
+		Addr:         fmt.Sprintf(":%d", srv.port),
+		Handler:      srv.RegisterRoutes(),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
-
-	return server
 }
